6849b1b140fba5b63240da8c2b5cec45a4d0260e
anish424 <anish424@gmail.com>
2018-07-29 23:38:02 +0200

    k
    k

---
+++ CustomerRepository.java
@@ -0,0 +1,9 @@
+package no.kantega.springandreact;
+
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CustomerRepository extends CrudRepository<STG_CCAR_STATISTICS, Long> {
+
+
+}
