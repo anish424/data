56149a350bcd3ca58db94b98f813c7b6b1177740
anish424 <anish424@gmail.com>
2018-07-30 08:11:04 +0000

    Delete CustomerRepository.java

--- CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package no.kantega.springandreact;
-
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface CustomerRepository extends CrudRepository<STG_CCAR_STATISTICS, Long> {
-
-
-}
